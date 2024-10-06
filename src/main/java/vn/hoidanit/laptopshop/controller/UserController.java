42fe8c9f1502389bcf3f85a6dd4b80b1351a9d0e
Phuchung166 <phuchung16062004@gmail.com>
2024-10-07 02:34:22 +0700

    update

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -12,7 +12,7 @@ import vn.hoidanit.laptopshop.service.UserService;
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -20,22 +20,22 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
+        model.addAttribute("eric", "test");
         model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 
-    @RequestMapping("/admin/user") //GET
+    @RequestMapping("/admin/user") // GET
     public String getUserPage(Model model) {
         model.addAttribute("newUser", new User());
         model.addAttribute("hoidanit", "from controller with model");
         return "admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST) //POST
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST) // POST
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
         System.out.println(" run here " + hoidanit);
+        this.userService.handleSaveUser(hoidanit);
         return "hello";
     }
 }
